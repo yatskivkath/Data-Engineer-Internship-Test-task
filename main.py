78c3c30d35b3294c3662eb657d0de0eb0dc39b5e
Yatskiv Kath <iamkate4@gmail.com>
2021-05-02 21:42:11 +0300

    added missing columns to dataframes

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 import pandas as pd
+import datetime as dt
+import re
 
 from download import download_files_list, download_file
 from upload import upload_file
 
 def main():
-    f = open("files_list.data", "r")
-    used_files = str.splitlines(f.read())
+    read = open("files_list.data", "r")
+    used_files = str.splitlines(read.read())
     new_files = download_files_list()
 
+    write = open("files_list.data", "a")
     for filename in new_files:
         if not filename in used_files:
-            df = pd.DataFrame.from_dict(download_file(filename))
+            send(pd.DataFrame.from_dict(download_file(filename)))
+            write.write(filename +'\n')
 
-            songs = convert(df, 'song')
-            movies = convert(df, 'movie')
-            apps = convert(df, 'app')
+            print(filename, " LOADED")
+    
+    write.close()
 
-            upload(songs, 'songs')
-            upload(movies, 'movies')
-            upload(apps, 'apps')
+            
+
+def send(df):
+    songs = convert(df, 'song')
+    movies = convert(df, 'movie')
+    apps = convert(df, 'app')
+
+    songs['ingestion_time'] = pd.to_datetime(dt.datetime.now())
+    movies['original_title_normalized'] = movies['original_title'].apply(lambda x: re.sub('[^0-9a-zA-Z_]+', '', x.casefold().replace(' ', '_') ))
+    apps['is_awesome'] = apps['rating'] >= 4.5 
+
+    upload_file(songs, 'songs')
+    upload_file(movies, 'movies')
+    upload_file(apps, 'apps')
 
 def convert(df, data_type):
     df = df.loc[df['type'] == data_type]
     df1 = pd.DataFrame(df['data'].values.tolist())
 
-    print(df1)
+    return df1
 
 if __name__ == "__main__":
     main()
--- upload.py
+++ upload.py
@@ -3,15 +3,13 @@ import sqlalchemy as sa
 
 from sqlalchemy import create_engine
 
-# engine = create_engine("mysql://USER:PASSWORD@HOST/DATABASE?charset=utf8mb4")
-# conn = engine.connect()
+engine = create_engine("mysql://USER:PASSWORD@34.123.31.139/DATABASE?charset=utf8mb4") # I used GCP SQL Instance
+conn = engine.connect()
 
 def upload_file(df, schema):
-    pass
-    
-    # try:
-    #     df.to_sql(schema, conn, if_exists = 'append', index = False)
-    # except sa.exc.IntegrityError:
-    #     pass
-    # except sa.exc.OperationalError:
-    #     pass
+    try:
+        df.to_sql(schema, conn, if_exists = 'append', index = False)
+    except sa.exc.IntegrityError:
+        pass
+    except sa.exc.OperationalError:
+        pass
